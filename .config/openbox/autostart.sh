63fee006624d5df5c1c3e6a9753462f1f648b9cc
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2011-04-26 20:43:53 -0300

    Adding more files

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,66 @@
+# This shell script is run before Openbox launches.
+# Environment variables set here are passed to the Openbox session.
+
+
+# functoon to star a given process without specific options
+function start_if_exist {
+	for process in $@; do
+		if which $process > /dev/null; then
+			$process &
+		fi
+	done
+}
+
+########## Start of default settings ###########
+
+# D-bus
+if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+       eval `dbus-launch --sh-syntax --exit-with-session`
+fi
+
+# Run XDG autostart things.  By default don't run anything desktop-specific
+# See xdg-autostart --help more info
+DESKTOP_ENV="OPENBOX"
+if which /usr/lib64/openbox/xdg-autostart >/dev/null; then
+  /usr/lib64/openbox/xdg-autostart $DESKTOP_ENV
+fi
+
+############# End default settings ###############
+
+############ Start of custom settings ############
+
+# starting ssh-agent
+eval $(ssh-agent)
+
+# setting correct keymap
+setxkbmap br
+
+# activating numlock key
+#numlockx on &
+
+# fixing fn keys
+xmodmap /home/ivan/.config/openbox/keys
+
+# wallpaper with feh
+WALLPAPER_PATH=/home/ivan/images/wallpaper
+feh --bg-scale $WALLPAPER_PATH
+
+# starting compositing
+#start_if_exist cairo-compmgr
+xcompmgr &
+
+# starting tint2, terminal emulator daemon, screensaver, conky
+# using tint2 instead of lxpanel
+start_if_exist tint2 wicd-gtk xfce4-power-manager
+
+xscreensaver -no-splash &
+
+# starting terminal
+if [[ -z $(pidof terminal) ]]; then
+	terminal --hide-menubar &
+fi
+
+# starting conky
+if [[ -n $DISPLAY && -z $(pidof conky) ]]; then
+	conky -c /home/ivan/.conkyrc_$(hostname) &
+fi
---
+++ .gdbinit
@@ -0,0 +1,24 @@
+## Setting default gdb configurations
+
+# stop printing string in the \0
+set print null-stop on
+
+# print a array in only one line
+set print array off
+
+# doesn't print the array indexes
+set print array-index off
+
+# prints the address a pointer refers to
+set print address on
+
+# pretty printing for C structs and unions
+set print pretty on
+set print union on
+
+## configuring scripts
+# don't print python stack trace
+maint set python print-stack
+
+# enabling script type detection
+set script-extension strict
