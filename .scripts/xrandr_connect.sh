a45a1582359e18c2d5a67e33e222d87ec1e87404
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2012-02-12 23:22:33 -0200

    Set laptop screen as primary

--- .scripts/xrandr_connect.sh
+++ .scripts/xrandr_connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 size=$(echo $(xrandr -q | grep VGA -A1 | tail -n 1) | cut -d " " -f 1)
-xrandr --output LVDS1 --mode 1280x800 --pos 72x${size/*x/} --rotate normal \
+xrandr --output LVDS1 --primary --mode 1280x800 --pos 72x${size/*x/} --rotate normal \
        --output VGA1 --mode $size --pos 0x0 --rotate normal
