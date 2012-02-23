256af160296055d298cbef7b099fd2b4cbb01fbd
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2012-02-22 23:26:35 -0200

    Make dzen stretch untill the right end

--- .scripts/dzenconky.sh
+++ .scripts/dzenconky.sh
@@ -3,4 +3,4 @@
 FG='white'
 BG='#121212'
 FONT='-*-terminus-*-*-*-*-12-*-*-*-*-*-iso8859-*'
-conky -c .conky_dzen | dzen2 -e - -x '840' -w '550' -ta r -fg $FG -bg $BG -fn $FONT &
+conky -c .conky_dzen | dzen2 -e - -x '840' -ta r -fg $FG -bg $BG -fn $FONT &
