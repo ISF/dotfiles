75b0a7342c6c1e137b7f0bd63acddbf0de09f144
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2012-03-28 15:22:41 -0300

    Using conky config per machine

--- .scripts/dzenconky.sh
+++ .scripts/dzenconky.sh
@@ -3,4 +3,4 @@
 FG='white'
 BG='#121212'
 FONT='-*-terminus-*-*-*-*-12-*-*-*-*-*-iso8859-*'
-conky -c $HOME/.conky_dzen | dzen2 -e - -x '840' -ta r -fg $FG -bg $BG -fn $FONT &
+conky -c $HOME/.conky_dzen_$(hostname) | dzen2 -e - -x '840' -ta r -fg $FG -bg $BG -fn $FONT &
