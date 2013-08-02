5b8f8d4d1718534c30192ca02fd446aaabdf429f
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2013-08-02 12:21:11 -0300

    Add options for external monitor location

--- .scripts/xrandr.sh
+++ .scripts/xrandr.sh
@@ -6,6 +6,12 @@ size_note=$(echo $(xrandr -q | grep LVDS1 -A1 | tail -n 1) | cut -d " " -f 1)
 if [[ -z $(xrandr -q | grep VGA | grep disconnected) ]]; then
     if [[ $1 == "extern_only" ]]; then
         xrandr --output LVDS1 --off --output VGA1 --mode $size --pos 0x0 --rotate normal
+    elif [[ $1 == "left" ]]; then
+        xrandr --output LVDS1 --primary --mode $size_note --pos 72x${size/*x/} --rotate normal \
+            --output VGA1 --mode $size --pos 0x0 --rotate normal --right-of VGA1
+    elif [[ $1 == "right" ]]; then
+        xrandr --output LVDS1 --primary --mode $size_note --pos 72x${size/*x/} --rotate normal \
+            --output VGA1 --mode $size --pos 0x0 --rotate normal --left-of VGA1
     else
         xrandr --output LVDS1 --primary --mode $size_note --pos 72x${size/*x/} --rotate normal \
             --output VGA1 --mode $size --pos 0x0 --rotate normal
