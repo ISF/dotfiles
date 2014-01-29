bff81e5cf9214032f81d6f7c34f59859f9f87054
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2014-01-29 17:50:33 -0200

    Use new options for offlineimap

--- .mail/mailrun.sh
+++ .mail/mailrun.sh
@@ -4,8 +4,8 @@ PID=$(pgrep offlineimap)
 
 # if it is still running then stop it
 if [[ -z "$PID" ]]; then
-    offlineimap -o -u quiet &>/dev/null &
-    sed -i 's/"+gmail/[Gmail]\.Spam"\|"+gmail/[Gmail]\.Trash"\|"+gmail/[Gmail]\.Sent Mail"|"+gmx/Spam"//' /home/ivan/.mutt/muttrc.mailboxes
+    offlineimap -o -a gmail,sdf -u quiet
+    sed -i 's@"+gmail/[Gmail]\.Spam"\|"+gmail/[Gmail]\.Trash"\|"+gmail/[Gmail]\.Sent Mail"|"+gmx/Spam"@@' /home/ivan/.mutt/muttrc.mailboxes
     goobook reload
     notmuch new
 fi
